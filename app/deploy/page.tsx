495b22e314126f19d57530fc81b8645e4d859ec0
celine94118 <celine.valente94118@gmail.com>
2025-07-15 14:33:43 +0200

    Delete app directory

--- app/deploy/page.tsx
+++
@@ -1,7 +0,0 @@
-// app/deploy/page.tsx
-// Cette page affiche le guide de déploiement final.
-import DeploymentFinalGuide from "@/components/deployment-final-guide"
-
-export default function DeployPage() {
-  return <DeploymentFinalGuide />
-}
--- app/paypal-setup/page.tsx
+++
@@ -1,5 +0,0 @@
-import PayPalSetupGuide from "@/components/paypal-setup-guide"
-
-export default function PayPalSetupPage() {
-  return <PayPalSetupGuide />
-}
